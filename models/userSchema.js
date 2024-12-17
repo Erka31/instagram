93bf7af96c9fd9a1cfb401a81e1f938b25cf3d98
24HP0212 24HP0212 <24HP0212@admins-iMac.local>
2024-12-17 18:09:00 +0800

    fixed profileimg

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
-    profileImg: { type: String, required: true },
+    profileImg: { type: String },
     bio: { type: String },
     posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
     following: [{ type: mongoose.Types.ObjectId, ref: "User" }],
