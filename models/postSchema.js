94b68c0cbe3a1a3eaf8db40bc9f03c2760386740
24HP0212 24HP0212 <24HP0212@admins-iMac.local>
2024-12-19 16:21:22 +0800

    corrected-postschema

--- models/postSchema.js
+++ models/postSchema.js
@@ -2,12 +2,11 @@ const { Schema, default: mongoose } = require("mongoose");
 
 const postSchema = new Schema(
   {
-    profileImg: { type: String, required: true },
     caption: { type: String, required: true },
     postImg: { type: String, required: true },
     userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-    comment: [{ type: mongoose.Types.ObjectId, ref: "Comment", required: true }],
-    like: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
+    comment: [{ type: mongoose.Types.ObjectId, ref: "Comment"}],
+    like: [{ type: mongoose.Types.ObjectId, ref: "User"}],
   },
   { timestamps: true }
 );
