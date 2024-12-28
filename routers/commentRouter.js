f0d73730c60ba653940e30031c77f0f378b47fd9
24HP0212 24HP0212 <24HP0212@admins-iMac.local>
2024-12-28 11:01:24 +0800

    latest commit

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -6,7 +6,7 @@ const {
   comments,
 } = require("../controllers/commentController");
 
-commentRoute.post("/comment/:id", comment);
+commentRoute.post("/comment/:postId", comment);
 commentRoute.post("/uncomment", uncomment);
 commentRoute.get("/getComment/:postId", comments);
 
